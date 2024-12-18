699caf8999265a633eacc009f8e99cd6d03c0da5
Mruhiil24 <84884354+Mruhiil24@users.noreply.github.com>
2024-12-18 11:35:54 -0800

    Update MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pymongo
 from pymongo import MongoClient
 
-df = open('/Users/sakshirathi/PycharmProjects/selenium/article_text.txt')
+df = open('/Users/monekaruhiil/PycharmProjects/selenium/article_text.txt')
 
 client =  MongoClient("mongodb+srv://moneka:Moneka2019!@cluster0.zgi2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client['dev']
